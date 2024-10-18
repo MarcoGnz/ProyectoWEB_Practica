cb325b51f9d5e3803ee7ef6ff5289032cf9f3bc1
Marco <marrco.gonzalez@gmail.com>
2024-10-18 00:54:03 -0300

    Vistas para filtros por categoría.

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,13 @@ from blog.models import Categoria, Post
 def blog(request):
     post = Post.objects.all()
     
+    
+    
     return render(request, 'blog/blog.html' , {"posts": post})
+
+def categoria(request, categoria_id ):
+    
+    categoria = Categoria.objects.get(id = categoria_id)
+    post = Post.objects.filter(categoria = categoria)
+    return render(request, "blog/categorias.html", {'categoria': categoria, "posts" : post})
 
